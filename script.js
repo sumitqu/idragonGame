4bbd8f9e1db342d0d3e0a3b970b09e29564fc192
Sumit Singh <70840698+sumitqu@users.noreply.github.com>
2024-07-02 16:35:40 +0530

    Update script.js
    introduced w a s d keys

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ setTimeout(() => {
 }, 1000);
 document.onkeydown = function (e) {
     console.log("Key code is: ", e.keyCode)
-    if (e.keyCode == 38) {
+    if (e.keyCode == 87 || e.keyCode==38) {
         dino = document.querySelector('.dino');
         dino.classList.add('animateDino');
         setTimeout(() => {
             dino.classList.remove('animateDino')
         }, 700);
     }
-    if (e.keyCode == 39) {
+    if (e.keyCode == 68 || e.keyCode==39) {
         dino = document.querySelector('.dino');
         dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
         dino.style.left = dinoX + 112 + "px";
     }
-    if (e.keyCode == 37) {
+    if (e.keyCode == 65 || e.keyCode==37) {
         dino = document.querySelector('.dino');
         dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
         dino.style.left = (dinoX - 112) + "px";
